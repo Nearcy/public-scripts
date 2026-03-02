1b0443605858c151d3ab1c33fe18fd589b7a7c39
Hayday <108716640+Nearcy@users.noreply.github.com>
2026-03-02 17:22:41 +0800

    Update claimed.py

--- claimed.py
+++ claimed.py
@@ -10,15 +10,15 @@ def fetch_image(filename):
         f.write(img_data)
 
 images = [
-    "claim_2_1.png","claim_2_2.png","claim_2_3.png","coupon.png"
+    "claim_1_1.png","claim_1_2.png","claim_2_1.png","claim_2_2.png","claim_2_3.png","coupon.png"
 ]
 
 for img in images:
     fetch_image(img)
 
-def checker():
+def checker_1_1():
     try:
-        match1 = pyautogui.locateOnScreen('ss1.png', confidence=0.8)
+        match1 = pyautogui.locateOnScreen('claim_1_1.png', confidence=0.8)
     except Exception as e:
         print(f"Error saat mencari ss1.png: {e}")
         match1 = None
@@ -26,61 +26,29 @@ def checker():
     if match1:
         try:
             x, y = pyautogui.center(match1)
-            pyautogui.click(x, y)
-            time.sleep(2)
-            pyautogui.moveTo(225, 30, duration=2)
-            pyautogui.doubleClick(225, 30)
-            time.sleep(1)
-            pyautogui.moveTo(15, 188, duration=2)
-            pyautogui.doubleClick(15, 188)
             time.sleep(1)
             return True
         except Exception as e:
             print(f"Error saat menjalankan proses match1: {e}")
             return False
+            
+def checker_1_2:
     try:
-        match2 = pyautogui.locateOnScreen('ss2.png', confidence=0.8)
+        match1 = pyautogui.locateOnScreen('claim_1_2.png', confidence=0.8)
     except Exception as e:
-        print(f"Error saat mencari ss2.png: {e}")
-        match2 = None
+        print(f"Error saat mencari ss1.png: {e}")
+        match1 = None
 
-    if match2:
+    if match1:
         try:
-            x, y = pyautogui.center(match2)
-            pyautogui.click(x, y)
+            x, y = pyautogui.center(match1)
+            pyautogui.doubleClick(x, y)
             time.sleep(2)
-            pyautogui.moveTo(225, 30, duration=2)
-            pyautogui.doubleClick(225, 30)
-            time.sleep(1)
-            pyautogui.moveTo(15, 188, duration=2)
-            pyautogui.doubleClick(15, 188)
-            time.sleep(1)
             return True
         except Exception as e:
-            print(f"Error saat menjalankan proses match2: {e}")
+            print(f"Error saat menjalankan proses match1: {e}")
             return False
-    try:
-        match3 = pyautogui.locateOnScreen('ss3.png', confidence=0.8)
-    except Exception as e:
-        print(f"Error saat mencari ss3.png: {e}")
-        match3 = None
-
-    if match3:
-        try:
-            x, y = pyautogui.center(match3)
-            pyautogui.click(x, y)
-            time.sleep(2)
-            pyautogui.moveTo(225, 30, duration=2)
-            pyautogui.doubleClick(225, 30)
-            time.sleep(1)
-            pyautogui.moveTo(15, 188, duration=2)
-            pyautogui.doubleClick(15, 188)
-            time.sleep(1)
-            return True
-        except Exception as e:
-            print(f"Error saat menjalankan proses match3: {e}")
-            return False
-        
+            
 def checker2():
     try:
         match1 = pyautogui.locateOnScreen('claim_2_1.png', confidence=0.8)
@@ -137,60 +105,6 @@ def checker3():
             return False
     print("Tidak ada mode yang cocok.")
     return False       
-        
-def checker_scroll(max_scroll=3000, step=200):
-    current_scroll = 0
-    while current_scroll <= max_scroll:
-        if checker():
-            print(f"Checker berhasil di posisi scroll {current_scroll}")
-            # tugas checker sudah dijalankan di dalam fungsi checker()
-            # jangan scroll balik ke atas, langsung lanjut ke bawah
-            current_scroll += step
-            pyautogui.scroll(-step)
-            print(f"Lanjut scroll ke bawah {current_scroll}")
-            time.sleep(1)
-        else:
-            current_scroll += step
-            print(f"Checker gagal, scroll ke bawah {current_scroll}")
-            pyautogui.scroll(-step)
-            time.sleep(1)
-    print("Checker tidak ditemukan lagi sampai batas scroll.")
-    
-def checker_scroll2(max_scroll=1800, step=200):
-    current_scroll = 0
-    while current_scroll <= max_scroll:
-        if checker2():
-            print(f"Checker berhasil di posisi scroll {current_scroll}")
-            # tugas checker sudah dijalankan di dalam fungsi checker()
-            # jangan scroll balik ke atas, langsung lanjut ke bawah
-            current_scroll += step
-            pyautogui.scroll(-step)
-            print(f"Lanjut scroll ke bawah {current_scroll}")
-            time.sleep(1)
-        else:
-            current_scroll += step
-            print(f"Checker gagal, scroll ke bawah {current_scroll}")
-            pyautogui.scroll(-step)
-            time.sleep(1)
-    print("Checker tidak ditemukan lagi sampai batas scroll.")
-
-def checker_scroll3(max_scroll=3000, step=200):
-    current_scroll = 0
-    while current_scroll <= max_scroll:
-        if checker3():
-            print(f"Checker berhasil di posisi scroll {current_scroll}")
-            # tugas checker sudah dijalankan di dalam fungsi checker()
-            # jangan scroll balik ke atas, langsung lanjut ke bawah
-            current_scroll += step
-            pyautogui.scroll(-step)
-            print(f"Lanjut scroll ke bawah {current_scroll}")
-            time.sleep(1)
-        else:
-            current_scroll += step
-            print(f"Checker gagal, scroll ke bawah {current_scroll}")
-            pyautogui.scroll(-step)
-            time.sleep(1)
-    print("Checker tidak ditemukan lagi sampai batas scroll.")
     
 def verif1():
     try:
@@ -278,7 +192,6 @@ def verif2():
     except Exception as e:
         print(f"Terjadi error: {e}")
 
-
 def move_and_click(start_x, start_y, end_x, end_y, duration=2):
     pyautogui.moveTo(start_x, start_y)
     time.sleep(0.5)
@@ -295,20 +208,17 @@ def modechoose():
 
     if match1:
         try:
-            time.sleep(10)
-            result = validation1()
-            print(f"Hasil validation1: {result}")
-            result2 = earnup()
-            print(f"Hasil earnup: {result2}")
-            result3 = coupon()
-            print(f"Hasil earnup: {result3}")
-            
-            pos = checker_scroll(max_scroll=3000, step=250)
-            if pos is not None:
-                print(f"Checker ditemukan, lanjut dari posisi scroll {pos}")
+            print("Berhasil match1_1")
+            if checker_1_1():
+                print("Checker_1_1 berhasil")
+                if checker_1_2():
+                    print("Checker_1_2 berhasil")
+                    pyautogui.hotkey('alt', 'f4') 
+                else:
+                    print("Checker_1_1 tidak ditemukan")
+                    pyautogui.hotkey('alt', 'f4') 
             else:
-                time.sleep(3)
-                pyautogui.hotkey('alt', 'f4')
+                print("ulang sampai ditemukan")
                 
             return True
         except Exception as e:
@@ -339,11 +249,8 @@ def modechoose():
             print(f"Error saat menjalankan proses match2: {e}")
             return False
         
-
     print("Tidak ada mode yang cocok.")
     return False
-
-        
         
 def run_sequence():
     start_x, start_y = 100, 100
